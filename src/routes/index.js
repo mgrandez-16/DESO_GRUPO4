b86f3a5c3aa54d8604afd0cab28175768e780ed5
Brendaatto <brenda.bpam.12@gmail.com>
2021-11-22 00:28:29 -0500

    index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,17 @@
-console.log("server works!");
+const express = require('express');
+const app = express();
+const path = require('path');
+
+//settings
+app.set('port', 3000);
+app.set('views',path.join(__dirname,'../views'));
+app.set('view engine', 'ejs');
+
+app.listen(app.get('port'));
+
+//middlewares
+
+//routes
+app.get('/',(req, res) => {
+    res.render('index');
+});
